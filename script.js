ba1b21339f621e208f29069364a632cfe6294744
Laís <luamoon.keke@gmail.com>
2026-09-10 10:25:06 -0400

    validacao de senha

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ formulario.addEventListener("submit", function(event) {
     formulario.style.display = "none";
     telaSenha.style.display = "block";
 });
+
+const continuarSenha = document.getElementById("continuarSenha");
+const senha = document.getElementById("senha");
+
+continuarSenha.addEventListener("click", function() {
+
+    if (senha.value.length < 8) {
+        alert("Erro: a senha precisa ter pelo menos 8 caracteres.");
+        return;
+    }
+
+    if (!/[A-Z]/.test(senha.value)) {
+        alert("Erro: a senha precisa conter uma letra maiúscula.");
+        return;
+    }
+
+    if (!/[0-9]/.test(senha.value)) {
+        alert("Erro: a senha precisa conter um número.");
+        return;
+    }
+
+    alert("Senha aceita!");
+});
